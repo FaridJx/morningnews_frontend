cd243a3e3070fa47e915b98ab9be28757e6482bc
Farid <farid.adinani@hotmail.com>
2023-10-04 15:36:11 +0200

    ajout de persistance

--- pages/_app.js
+++ pages/_app.js
@@ -3,24 +3,35 @@ import Head from 'next/head';
 import Header from '../components/Header';
 
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import bookmarks from '../reducers/bookmarks';
 import user from '../reducers/user';
 import hiddenArticles from '../reducers/hiddenArticles';
 
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
+const reducers = combineReducers({ bookmarks, user, hiddenArticles });
+const persistConfig = { key: 'morningNews', storage };
 
 const store = configureStore({
-  reducer: { bookmarks, user, hiddenArticles },
+  reducer : persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 });
+
+const persistor = persistStore(store);
 
 function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <Head>
         <title>Morning News</title>
       </Head>
       <Header />
       <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
